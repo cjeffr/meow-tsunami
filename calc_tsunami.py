b0d3b403b88b8787971470b3e3a3abc0299d5a32
Catherine Jeffries <jeffriesc@cwu.edu>
2018-06-01 18:28:56 -0700

    Fixed where calc_tsunami wasn't a class

--- calc_tsunami.py
+++ calc_tsunami.py
@@ -18,60 +18,61 @@ class SlipCalc():
         self.gauge_file = model['gauges']
         self.catalog = model['catalog']
         
-def get_array_size():
-    """
-    Defines the size of the array based on the number of tide gauges tracked and the number of subfaults
+    def get_array_size(self):
+        """
+        Defines the size of the array based on the number of tide gauges tracked and the number of subfaults
 
-    Returns
-    -------
-    tg_nbr = the number of tide gauges tracked
+        Returns
+        -------
+        tg_nbr = the number of tide gauges tracked
 
-    """
-    tg_file = 'NA_CAS_gauges.txt'
-    lines = open(tg_file).readlines()
-    tg_nbr = len(lines)
-    return tg_nbr
+        """
+        tg_file = self.gauge_file #'NA_CAS_gauges.txt'
+        lines = open(tg_file).readlines()
+        tg_nbr = len(lines)
+        return tg_nbr
 
 
-def calc_tsunami(slip_result):
-    """
-    This function calculates the tsunami sized from slip
+    def calc_tsunami(self, slip_result):
+        """
+        This function calculates the tsunami sized from slip
 
-    Parameters
-    ----------
-    slip_result: The slip array obtained from RabbitMQ for each model
+        Parameters
+        ----------
+        slip_result: The slip array obtained from RabbitMQ for each model
 
-    Returns:
-    -------
-    waveheight_per_site: the new tGF array for each location
-    time array:  time array
+        Returns:
+        -------
+        waveheight_per_site: the new tGF array for each location
+        time array:  time array
 
-   """
-    gf = h5py.File('NA_CAS.hdf5', 'r')
-    time_array = np.array(gf['time/timedata'])
+       """
+        gf = h5py.File(self.catalog, 'r') # h5py.File('NA_CAS.hdf5', 'r')
+        time_array = np.array(gf['time/timedata'])
 
-    # dictionary for holding slip calculations
-    scale_gf = []
+        # dictionary for holding slip calculations
+        scale_gf = []
 
-    # declare empty array with max size
-    ar_len = len(time_array)
-    ar_width = get_array_size()
+        # declare empty array with max size
+        ar_len = len(time_array)
 
-    tgf = np.zeros(shape=(ar_len, ar_width)) # tgf = tsunami green's function
+        ar_width = self.get_array_size()
 
-    # loop over index adn slip value from slip array
-    for i, slip in enumerate(slip_result):
-        # print(i)
-        # make sure slip is a float not string
-        s = float(slip)
+        tgf = np.zeros(shape=(ar_len, ar_width)) # tgf = tsunami green's function
 
-        # multiply slip by each subfault
-        scale_gf.append(s * gf['GF/{:03}'.format(i)][:])
+        # loop over index adn slip value from slip array
+        for i, slip in enumerate(slip_result):
+            # print(i)
+            # make sure slip is a float not string
+            s = float(slip)
 
-    # iterate over all the subfaults and add all subfaults together per site
-    for sf in scale_gf:
-        tgf += sf
+            # multiply slip by each subfault
+            scale_gf.append(s * gf['GF/{:03}'.format(i)][:])
 
-    # return the slip_at_site array and the time array
-    return (tgf, time_array)
+        # iterate over all the subfaults and add all subfaults together per site
+        for sf in scale_gf:
+            tgf += sf
+
+        # return the slip_at_site array and the time array
+        return (tgf, time_array)
 
